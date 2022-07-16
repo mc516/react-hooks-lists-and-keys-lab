ab077d30706db43eb0fea65494be6898c011bb91
Mickey <mickeyc11111@gmail.com>
2022-07-15 22:09:55 -0400

    donezo

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -6,7 +6,7 @@ function ProjectList({ projects }) {
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{ projects.map(project => {return <ProjectItem key={project.id} technologies={project.technologies} name={project.name} about={project.about}/>})/* render ProjectItem components here */}</div>
     </div>
   );
 }
